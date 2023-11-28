2c8869d3b40aefaab3beca00affcc7b12dd9e95a
Brandie Lucano <blucano.dev@gmail.com>
2023-11-28 12:34:52 -0800

    completed 17-20

--- 17_mode.py
+++ 17_mode.py
@@ -11,3 +11,14 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    counts = {}
+    max = 0
+    # counts{1: 2, 2: 1}
+
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+        # if(counts[num] > max):
+        #     max = num
+        max = num if counts[num] > max else max
+    return max
+
